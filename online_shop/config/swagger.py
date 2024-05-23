ed10a25ff53bb5c15196b745365d26a5f87ee19b
KhSanzhar <sanzhkhalel@gmail.com>
2024-05-23 10:36:55 +0500

    swagger_2

--- online_shop/config/swagger.py
+++ online_shop/config/swagger.py
@@ -1,16 +1,35 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.urls import path
+
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Online Shop API",
         default_version='v1',
-        description="API documentation for the Online Shop project",
+        description="This is the API documentation for the Online Shop project. Here you can find all the endpoints and their descriptions.",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@online_shop.local"),
+        contact=openapi.Contact(email="support@onlineshop.local"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
+
+# from drf_yasg.renderers import SwaggerUIRenderer
+#
+# class MySwaggerRenderer(SwaggerUIRenderer):
+#     def get_customizations(self):
+#         customizations = super().get_customizations()
+#         customizations['displayOperationId'] = True
+#         customizations['defaultModelExpandDepth'] = 2
+#         return customizations
+#
+# urlpatterns = [
+#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0, renderer_classes=[MySwaggerRenderer]), name='schema-swagger-ui'),]
+
--- online_shop/shop/views.py
+++ online_shop/shop/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from .models import Category, Product, Customer, Order, OrderItem, Review
 from .serializers import CategorySerializer, ProductSerializer, CustomerSerializer, OrderSerializer, OrderItemSerializer, ReviewSerializer
 
+from rest_framework.decorators import api_view, schema
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -30,3 +35,34 @@ class ReviewViewSet(viewsets.ModelViewSet):
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'shop/products.html', {'products': products})
+
+
+@swagger_auto_schema(
+    method='get',
+    manual_parameters=[
+        openapi.Parameter('category', openapi.IN_QUERY, description="Category ID", type=openapi.TYPE_INTEGER)
+    ],
+    responses={200: openapi.Response('A list of products', ProductSerializer(many=True))}
+)
+@api_view(['GET'])
+def product_list(request):
+    category_id = request.GET.get('category')
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+# @swagger_auto_schema(
+#     method='get',
+#     operation_description="Retrieve a list of products",
+#     responses={200: openapi.Response('A list of products', ProductSerializer(many=True))}
+# )
+#
+# @api_view(['GET'])
+# def product_list(request):
+#     products = Product.objects.all()
+#     serializer = ProductSerializer(products, many=True)
+#     return Response(serializer.data)
